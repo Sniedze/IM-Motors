6e6727c8bcf76ed03e21e256a71e9f97dfff4167
RytisDr <r.drazdauskas@gmail.com>
2019-04-22 20:58:31 +0200

    creates Subpage.jsx

---
+++ im-motors/src/components/Subpage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+export default class Subpage extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
