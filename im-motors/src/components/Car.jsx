833bab93b2649103ca5f00e25c9ea987c45f7e55
RytisDr <r.drazdauskas@gmail.com>
2019-05-02 00:14:42 +0200

    Outputs specific info, needs to stop displaying all.

--- im-motors/src/components/Car.jsx
+++ im-motors/src/components/Car.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Inventory from "./Inventory.jsx";
 
 export default class Car extends Component {
   constructor() {
@@ -18,18 +15,14 @@ export default class Car extends Component {
       .then(e => e.json())
       .then(car => {
         this.setState({ data: car });
-
-        console.log(this.state.data);
       });
   }
 
   render() {
-    if (this.state.data) {
-      return (
-        <div>
-          <h1>{this.state.data.name}</h1>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <h1>{this.state.data.name}</h1>
+      </div>
+    );
   }
 }
--- im-motors/src/components/Inventory.jsx
+++ im-motors/src/components/Inventory.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import { NavLink, Route } from "react-router-dom";
+import { NavLink, Route, Switch, HashRouter } from "react-router-dom";
 import CarInfo from "./Car.jsx";
 //import { Switch, Route, HashRouter } from "react-router-dom";
 //import Subpage from "./Subpage.jsx";
 
 export default class Inventory extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       data: []
     };
+    //this.handler = this.handler.bind(this);
   }
   componentDidMount() {
     fetch(`https://5be00dbef2ef840013994a6d.mockapi.io/users/`)
@@ -20,10 +21,20 @@ export default class Inventory extends Component {
         this.setState({ data: result });
       });
   }
+
+  /*   handler() {
+    this.setState({
+      fullInventory: !false
+    });
+  } */
   render() {
     let person = this.state.data.map(item => (
       <>
-        <NavLink to={`/inventory/${item.id}`} activeClassName="active">
+        <NavLink
+          to={`/inventory/${item.id}`}
+          activeClassName="active"
+          /* onClick={this.handler} */
+        >
           <div key={item.id}>
             <h1 key={item.createdAt}>{item.name}</h1>
             <img key={item.comment} src={item.avatar} alt="Profile" />
@@ -31,17 +42,15 @@ export default class Inventory extends Component {
         </NavLink>
       </>
     ));
-    return (
-      <div>
-        <Header />
-        <Route
-          path="/inventory/:carId"
-          component={CarInfo}
-          props={this.state.data}
-        />
-        {person}
-        <Footer />
-      </div>
-    );
+    {
+      return (
+        <div>
+          <Header />
+          <Route path="/inventory/:carId" component={CarInfo} />
+          {person}
+          <Footer />
+        </div>
+      );
+    }
   }
 }
