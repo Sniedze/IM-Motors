c909395b68b2ee7fb27eade5c480ea0a7061699b
Ulrika <u.sniedze@gmail.com>
2019-05-22 15:07:49 +0200

    Review component fetches posted reviews and displays them in the website

--- im-motors/src/components/Home.jsx
+++ im-motors/src/components/Home.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import Reviews from "./Reviews";
+import Reviews from "./reviews/Reviews";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      reviews: ""
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <div>
+      <div className="home">
         <Header />
         <div className="membership">
           <p>Become a member and get free cinema tickets</p>
         </div>
-        <Reviews reviews={this.state.reviews} />
+        <Reviews />
         <Footer />
       </div>
     );
