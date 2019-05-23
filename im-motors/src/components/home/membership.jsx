e4a250afdc3110040c85e6d2ec37e458cfd279c8
Elaheh <elah0048@stud.kea.dk>
2019-05-23 12:30:24 +0200

    Membership form,react router

--- im-motors/src/components/home/membership.jsx
+++ im-motors/src/components/home/membership.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 class Membership extends Component {
   state = {};
   render() {
     return (
       <div className="container">
-        <h2>Become a member </h2>
-        <ul>
-          <li>- Discount for the first purchase</li>
-          <li>- Get free cinema tickets</li>
-          <li>- Collect loyalty points</li>
-        </ul>
+        <Link to="/membership">
+          <h2>Become a member </h2>
+          <ul>
+            <li>- Discount for the first purchase</li>
+            <li>- Get free cinema tickets</li>
+            <li>- Collect loyalty points</li>
+          </ul>
+        </Link>
       </div>
     );
   }
---
+++ im-motors/src/components/home/membershipForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import UserContact from "../contactForm.jsx";
+import { Link } from "react-router-dom";
+
+class MembershipForm extends Component {
+  state = {};
+  render() {
+    return <UserContact />;
+  }
+}
+
+export default MembershipForm;
