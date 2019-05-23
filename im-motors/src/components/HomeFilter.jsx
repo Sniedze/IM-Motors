51f8f16717eeb129b2d373f06af9acea9b3dcf73
RytisDr <r.drazdauskas@gmail.com>
2019-05-23 17:22:54 +0200

    Prepares option selection for onChange.

--- im-motors/src/components/HomeFilter.jsx
+++ im-motors/src/components/HomeFilter.jsx
@@ -4,25 +4,29 @@ export default class HomeFilter extends Component {
   constructor() {
     super();
     this.state = {
+      endpoint: "https://immotors-65ac.restdb.io/rest/cars",
+      fetchSettings: {
+        async: true,
+        crossDomain: true,
+        method: "GET",
+        headers: {
+          "cache-control": "no-cache",
+          "content-type": "application/json",
+          "x-apikey": "5ce2d6b1780a473c8df5c9ef"
+        }
+      },
       data: [],
       selections: { make: null, model: null, year: null }
     };
   }
   componentDidMount() {
-    const endP = "https://immotors-65ac.restdb.io/rest/cars";
-    const fetchSettings = {
-      async: true,
-      crossDomain: true,
-      method: "GET",
-      headers: {
-        "cache-control": "no-cache",
-        "content-type": "application/json",
-        "x-apikey": "5ce2d6b1780a473c8df5c9ef"
-      }
-    };
-    fetch(endP, fetchSettings).then(e =>
+    fetch(this.state.endpoint, this.state.fetchSettings).then(e =>
       e.json().then(result => this.setState({ data: result }))
     );
+  }
+  handleChange(value) {
+    // this.state.selections.setState({ make: value }); fix this to set state of make
+    //console.log(this.state.selections.make);
   }
   render() {
     //map out each fetched item's property, weed out repetition with Set(), turn it back into array
@@ -31,13 +35,14 @@ export default class HomeFilter extends Component {
     );
     //map out options for dropdown menu
     const makerOptions = uniqueMakerList.map(item => (
-      <option key={item}>{item}</option>
+      <option key={item} value={item}>
+        {item}
+      </option>
     ));
 
     return (
-      <select
-        className="hFilterDropdown make" /* onClick={fetch with parameter} */
-      >
+      <select className="hFilterDropdown make" onChange={this.handleChange}>
+        <option value={null} label="-Make-" />
         {makerOptions}
       </select>
     );
