59941dea15307eda4681925dfa35f0e505306e8e
Elaheh <elah0048@stud.kea.dk>
2019-05-28 22:09:32 +0200

    Review component has been styled,google map has been added to the footer

--- im-motors/src/components/BurgerMenu/slideDrawer.jsx
+++ im-motors/src/components/BurgerMenu/slideDrawer.jsx
@@ -9,38 +9,38 @@ const slideDrawer = props => {
   }
   return (
     <div>
-      <nav className={drawerClasses}>
-        <ul>
-          <li>
-            <NavLink to="/" exact activeClassName="active">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/inventory" activeClassName="active">
-              Inventory
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/finance" activeClassName="active">
-              Finance
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" activeClassName="active">
-              About us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/blog" activeClassName="active">
-              Blog
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <button className="close-button">
-        <i className="fas fa-times" />
-      </button>
+      <div className={drawerClasses}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" exact activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/inventory" activeClassName="active">
+                Inventory
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/finance" activeClassName="active">
+                Finance
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="active">
+                About us
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog" activeClassName="active">
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <CloseButton closeMenu={props.close} />
+      </div>
     </div>
   );
 };
--- im-motors/src/components/home/Home.jsx
+++ im-motors/src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Footer from "../Footer.jsx";
 import Membership from "./membership";
-import TeenagerOffers from "./TeenagerOffers";
 import Reviews from "../reviews/Reviews";
 import HomeFilter from "./filter/HomeFilter";
 
@@ -12,7 +11,6 @@ export default class Home extends Component {
         <Membership />
         <HomeFilter />
         <Reviews />
-        <Footer />
       </div>
     );
   }
