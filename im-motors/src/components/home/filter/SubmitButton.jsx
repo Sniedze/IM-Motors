a12785a355a5e975abb310e4438c4b56afc105db
RytisDr <r.drazdauskas@gmail.com>
2019-05-28 12:34:02 +0200

    Redirects to inventory page with url parameter.

--- im-motors/src/components/home/filter/SubmitButton.jsx
+++ im-motors/src/components/home/filter/SubmitButton.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+//import Inventory from "../../Inventory";
 export default class SubmitButton extends Component {
   /* constructor(props) {
     super(props);
   } */
+  state = {
+    redirect: ""
+  };
   handleSubmit = () => {
     if (this.props.make) {
-      /*       <Switch>
-        <Route path="/inventory"/>
-      </Switch>; */
+      this.setState({ redirect: true });
     } else {
       return null;
     }
   };
   render() {
+    if (this.state.redirect) {
+      return <Redirect push to={`/inventory?=${this.props.make}`} />;
+    }
+    /*     let route = (
+      <Switch>
+        <Link to={`/inventory`} />
+        <Route path="/inventory" component={Inventory} />
+      </Switch>
+    ); */
     return (
       <button id="hFilterSubmit" onClick={this.handleSubmit}>
         Go!
