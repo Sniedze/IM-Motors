1664be37c950c8d98523098d04b97c48d70f528a
RytisDr <r.drazdauskas@gmail.com>
2019-05-29 19:54:13 +0200

    Creates Categories filter component for home page.

--- im-motors/src/components/home/Home.jsx
+++ im-motors/src/components/home/Home.jsx
@@ -3,12 +3,13 @@ import Footer from "../Footer.jsx";
 import Membership from "./membership";
 import Reviews from "../reviews/Reviews";
 import HomeFilter from "./filter/HomeFilter";
-
+import Categories from "./Categories";
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         <Membership />
+        <Categories />
         <HomeFilter />
         <Reviews />
       </div>
