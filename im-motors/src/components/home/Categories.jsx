a3f68df14dc984741612ea034e00ca501bfc1742
RytisDr <r.drazdauskas@gmail.com>
2019-05-30 11:32:03 +0200

    Categories component added.

---
+++ im-motors/src/components/home/Categories.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Categories extends Component {
+  render() {
+    return (
+      <div className="catContainer">
+        <h1>Vehicles by Category</h1>
+      </div>
+    );
+  }
+}
