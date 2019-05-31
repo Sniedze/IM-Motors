2d9b3a03801e08da258dbcfe6bdfa669bab69f15
Ulrika <u.sniedze@gmail.com>
2019-05-31 15:20:41 +0200

    Links logo to Home component

--- im-motors/src/components/Logo.jsx
+++ im-motors/src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import MotorsLogo from "../assets/blacklogo.png";
-
+import { Link } from "react-router-dom";
 export default class Logo extends Component {
   render() {
     return (
-      <img className="logoImage" src={MotorsLogo} alt={"IM Motors logo"} />
+      <Link to="/">
+        <img className="logoImage" src={MotorsLogo} alt={"IM Motors logo"} />
+      </Link>
     );
   }
 }
