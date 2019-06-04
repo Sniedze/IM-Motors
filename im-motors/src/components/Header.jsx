637d32845863b443b32d8f179b76343b87ec6c23
Ulrika <u.sniedze@gmail.com>
2019-06-04 14:35:39 +0200

    Adds grid to About Us page

--- im-motors/src/components/Header.jsx
+++ im-motors/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default class Header extends Component {
         <Logo />
         <Menu />
         <ToggleButton click={this.props.handleSlide} />
-        <h5>We sell safe cars</h5>
+       
       </header>
     );
   }
