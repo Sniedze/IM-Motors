1ad8376e0a4d0df6417bc9e20e6a92b4ae182c71
Ulrika <u.sniedze@gmail.com>
2019-06-05 11:49:10 +0200

    Expands membership container

--- im-motors/src/components/home/Home.jsx
+++ im-motors/src/components/home/Home.jsx
@@ -9,18 +9,16 @@ import LandingPFooter from "./landingPFooter";
 export default class Home extends Component {
   render() {
     return (
-      <div className="home-wrapper">
-        <div className="home">
-          <Membership />
-          <TeenagerOffers />
-          <Categories />
-          <HomeFilter />
-          <Reviews />
-          <LandingPFooter />
-        </div>
-        <div className="copyright">
-          <p>Copyright © IM Motors 2019. All rights reserved.</p>
-        </div>
+      <div className="home">
+        <Membership />
+        <TeenagerOffers />
+        <Categories />
+        <HomeFilter />
+        <Reviews />
+        <LandingPFooter />
+        <p className="copyright">
+          Copyright © IM Motors 2019. All rights reserved.
+        </p>
       </div>
     );
   }
